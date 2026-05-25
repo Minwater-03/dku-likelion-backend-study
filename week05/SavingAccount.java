00a08d5d956592bd8ae50f94272030026b2e79fa
Minwater-03 <minsoo6505@naver.com>
2026-05-25 16:33:48 +0900

    week05 실습 자료 및 정리

--- week05/SavingAccount.java
+++
@@ -1,7 +0,0 @@
-public class SavingAccount extends BankAccount implements Withdrawable {
-    boolean isOverdraft;
-    void transfer(){};
-    public void withdraw(){
-        System.out.println("Withdrawing");
-    };
-}
--- week05/SubscriptionAccount.java
+++
@@ -1,3 +0,0 @@
-public class SubscriptionAccount extends BankAccount{
-    int numOfSubscription;
-}
